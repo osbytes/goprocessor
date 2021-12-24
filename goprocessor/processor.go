65eca81f6a4c1ecdbe060caefb99b77533393af3
dillon <dillonstreator@gmail.com>
2021-12-24 09:52:38 -0600

    go processor with generics

---
+++ goprocessor/processor.go
@@ -0,0 +1,93 @@
+package goprocessor
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/avast/retry-go"
+)
+
+type Options struct {
+	MaxConcurrentItems int
+	RetryMaxPerItem    uint
+	RetryLastErrorOnly bool
+	GracefulShutdown   bool
+}
+
+func Process[T any](ctx context.Context, items []T, fn func(item T) error, opts *Options) []error {
+	maxConcurrent := 5
+	maxRetriesPerItem := uint(0)
+	gracefulShutdown := false
+	retryLastErrorOnly := true
+
+	if opts != nil {
+		if opts.MaxConcurrentItems > 0 {
+			maxConcurrent = opts.MaxConcurrentItems
+		}
+
+		maxRetriesPerItem = opts.RetryMaxPerItem
+		gracefulShutdown = opts.GracefulShutdown
+		retryLastErrorOnly = opts.RetryLastErrorOnly
+	}
+
+	boundedCh := make(chan struct{}, maxConcurrent)
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	errs := []error{}
+	syncAddErr := func(err error) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		errs = append(errs, err)
+	}
+
+	for _, item := range items {
+		boundedCh <- struct{}{}
+
+		if len(errs) > 0 {
+			break
+		}
+
+		it := item
+
+		wg.Add(1)
+		go func() {
+			defer func() {
+				wg.Done()
+				<-boundedCh
+			}()
+
+			select {
+			case <-ctx.Done():
+				syncAddErr(ctx.Err())
+				return
+
+			default:
+				if maxRetriesPerItem > 0 {
+					fmt.Println(maxRetriesPerItem)
+					err := retry.Do(func() error {
+						return fn(it)
+					}, retry.Attempts(maxRetriesPerItem), retry.LastErrorOnly(retryLastErrorOnly)) // TOOD: allow more configuration on this retry
+					if err != nil {
+						syncAddErr(err)
+					}
+
+				} else {
+
+					if err := fn(it); err != nil {
+						syncAddErr(err)
+					}
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if len(errs) > 0 && !gracefulShutdown {
+		return []error{errs[0]}
+	}
+
+	return errs
+}
---
+++ goprocessor/processor_test.go
@@ -0,0 +1,143 @@
+package goprocessor
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcess(t *testing.T) {
+	assert := assert.New(t)
+
+	processed := map[string]bool{}
+	mu := sync.Mutex{}
+
+	errs := Process(
+		context.Background(),
+		[]string{"a", "b", "c"},
+		func(item string) error {
+			mu.Lock()
+			processed[item] = true
+			mu.Unlock()
+
+			return nil
+		},
+		nil,
+	)
+	assert.Len(errs, 0)
+
+	assert.Contains(processed, "a")
+	assert.Contains(processed, "b")
+	assert.Contains(processed, "c")
+}
+
+func TestProcess_errors(t *testing.T) {
+	assert := assert.New(t)
+
+	err1 := errors.New("error1")
+	err2 := errors.New("error2")
+	err3 := errors.New("error3")
+
+	errs := Process(
+		context.Background(),
+		[]string{"a", "b", "c"},
+		func(item string) error {
+			switch item {
+			case "a":
+				return err1
+			case "b":
+				return err2
+			case "c":
+				return err3
+			}
+			return nil
+		},
+		&Options{MaxConcurrentItems: 1},
+	)
+	assert.Len(errs, 1)
+	assert.ErrorIs(err1, errs[0])
+}
+
+func TestProcess_errors_graceful(t *testing.T) {
+	assert := assert.New(t)
+
+	err1 := errors.New("error1")
+	err2 := errors.New("error2")
+	err3 := errors.New("error3")
+
+	errs := Process(
+		context.Background(),
+		[]string{"a", "b", "c"},
+		func(item string) error {
+			switch item {
+			case "a":
+				return err1
+			case "b":
+				return err2
+			case "c":
+				return err3
+			}
+			return nil
+		},
+		&Options{MaxConcurrentItems: 3, GracefulShutdown: true},
+	)
+	assert.Len(errs, 3)
+	assert.Contains(errs, err1)
+	assert.Contains(errs, err2)
+	assert.Contains(errs, err3)
+}
+
+func TestProcess_retries_success(t *testing.T) {
+	assert := assert.New(t)
+
+	attemptNum := uint(1)
+	maxAttempts := uint(3)
+
+	errs := Process(
+		context.Background(),
+		[]string{"a"},
+		func(item string) error {
+			if attemptNum < maxAttempts {
+				attemptNum++
+				return errors.New("err")
+			}
+
+			return nil
+		},
+		&Options{RetryMaxPerItem: maxAttempts},
+	)
+	assert.Len(errs, 0)
+}
+
+func TestProcess_retries_error(t *testing.T) {
+	assert := assert.New(t)
+
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	num := strconv.Itoa(seededRand.Int())
+
+	err := fmt.Errorf(num)
+	attemptNum := uint(1)
+
+	errs := Process(
+		context.Background(),
+		[]string{"a"},
+		func(item string) error {
+			if attemptNum < 3 {
+				attemptNum++
+				return err
+			}
+
+			return nil
+		},
+		&Options{RetryMaxPerItem: 2, RetryLastErrorOnly: false},
+	)
+	assert.Len(errs, 1)
+	assert.Contains(errs[0].Error(), num)
+}
